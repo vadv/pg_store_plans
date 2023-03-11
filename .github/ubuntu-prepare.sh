cf369d43a761b653a237a5941db1b62c0072102d
Dmitry Vasiliev <dmitrivasilyev@ozon.ru>
2023-03-11 16:23:10 +0300

    build deb package

---
+++ .github/ubuntu-prepare.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+
+sudo apt-get update
+sudo apt-get install -y git curl ca-certificates gnupg lsb-release build-essential
+curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/apt.postgresql.org.gpg > /dev/null
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+sudo apt-get update
+sudo apt-get install -y postgresql-$PG_VERSION postgresql-server-dev-$PG_VERSION
+export PG_CONFIG=/usr/lib/postgresql/$PG_VERSION/bin/pg_config
+make
