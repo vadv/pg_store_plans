615db10f0173c2ada32ae01008ffb7c100ed9adf
Vasilyev Dmitriy Viktorovich <dmitrivasilyev@ozon.ru>
2025-06-07 14:29:58 +0300

    fix regression

--- sql/vadv.sql
+++ sql/vadv.sql
@@ -1,7 +1,7 @@
 SET client_min_messages = 'error';
 CREATE EXTENSION IF NOT EXISTS pg_store_plans;
 CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
-SELECT pg_stat_statements_reset();
+SELECT pg_stat_statements_reset() IS NOT NULL AS t;
 SELECT pg_store_plans_reset(); -- collision with plan id
 SELECT 'sleep' as regress, pg_sleep(0.1);
 SELECT sum(p.calls) FROM pg_stat_statements s JOIN pg_store_plans p ON (s.queryid = p.queryid_stat_statements) JOIN pg_database d ON (d.oid = s.dbid) WHERE  s.query = 'SELECT $1 as regress, pg_sleep($2)';
