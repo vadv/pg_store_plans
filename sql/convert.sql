96870717fb5a8739a9ac8334ffa077d584b701b1
dmitrivasilyev <dmitrivasilyev@ozon.ru>
2026-06-22 07:11:10 +0300

    Починить convert regression для старых PostgreSQL

--- sql/convert.sql
+++ sql/convert.sql
@@ -1,63 +1,67 @@
 SET client_min_messages = 'error';
 CREATE EXTENSION IF NOT EXISTS pg_store_plans;
 
-WITH plans(title, tag, lplan) AS (
-  VALUES
-    ('incremental sort', '"t":"C"', $${
-      "Plan": {
-        "Node Type": "Incremental Sort",
-        "Async Capable": false,
-        "Disabled": false,
-        "Presorted Key": ["a"],
-        "Sort Key": ["a", "b"],
-        "Plans": [
-          {
-            "Node Type": "Seq Scan",
-            "Relation Name": "t1",
-            "Alias": "t1"
-          }
-        ]
-      }
-    }$$),
-    ('memoize', '"t":"E"', $${
-      "Plan": {
-        "Node Type": "Memoize",
-        "Async Capable": false,
-        "Disabled": false,
-        "Plans": [
-          {
-            "Node Type": "Index Scan",
-            "Scan Direction": "Forward",
-            "Index Name": "t1_a_idx",
-            "Relation Name": "t1",
-            "Alias": "t1"
-          }
-        ]
-      }
-    }$$),
-    ('tid range scan', '"t":"D"', $${
-      "Plan": {
-        "Node Type": "Tid Range Scan",
-        "Async Capable": false,
-        "Disabled": false,
-        "Relation Name": "t1",
-        "Alias": "t1"
-      }
-    }$$)
-),
-converted AS (
-  SELECT title, tag, lplan, pg_store_plans_shorten(lplan) AS splan
-  FROM plans
-)
-SELECT title,
-       splan LIKE '%' || tag || '%' AS node_shortened,
-       splan LIKE '%"ds":false%' AS disabled_shortened,
-       pg_store_plans_jsonplan(splan)::jsonb = lplan::jsonb AS json_round_trip,
-       pg_store_plans_textplan(splan) IS NOT NULL AS text_ok,
-       pg_store_plans_yamlplan(splan) IS NOT NULL AS yaml_ok,
-       pg_store_plans_xmlplan(splan) IS NOT NULL AS xml_ok
-FROM converted
-ORDER BY title;
+COPY (
+  WITH plans(title, min_version, tag, lplan) AS (
+    VALUES
+      ('incremental sort', 130000, '"t":"C"', $${
+        "Plan": {
+          "Node Type": "Incremental Sort",
+          "Async Capable": false,
+          "Disabled": false,
+          "Presorted Key": ["a"],
+          "Sort Key": ["a", "b"],
+          "Plans": [
+            {
+              "Node Type": "Seq Scan",
+              "Relation Name": "t1",
+              "Alias": "t1"
+            }
+          ]
+        }
+      }$$),
+      ('memoize', 140000, '"t":"E"', $${
+        "Plan": {
+          "Node Type": "Memoize",
+          "Async Capable": false,
+          "Disabled": false,
+          "Plans": [
+            {
+              "Node Type": "Index Scan",
+              "Scan Direction": "Forward",
+              "Index Name": "t1_a_idx",
+              "Relation Name": "t1",
+              "Alias": "t1"
+            }
+          ]
+        }
+      }$$),
+      ('tid range scan', 140000, '"t":"D"', $${
+        "Plan": {
+          "Node Type": "Tid Range Scan",
+          "Async Capable": false,
+          "Disabled": false,
+          "Relation Name": "t1",
+          "Alias": "t1"
+        }
+      }$$)
+  ),
+  converted AS (
+    SELECT title, min_version, tag, lplan, pg_store_plans_shorten(lplan) AS splan
+    FROM plans
+  )
+  SELECT title,
+         (splan LIKE '%' || tag || '%') =
+           (current_setting('server_version_num')::int >= min_version)
+           AS node_shortened_for_version,
+         splan LIKE '%"ds":false%' AS disabled_shortened,
+         pg_store_plans_jsonplan(splan)::jsonb = lplan::jsonb AS json_round_trip,
+         pg_store_plans_textplan(splan) IS NOT NULL AS text_ok,
+         pg_store_plans_yamlplan(splan) IS NOT NULL AS yaml_ok,
+         pg_store_plans_xmlplan(splan) IS NOT NULL AS xml_ok
+  FROM converted
+  ORDER BY title
+) TO STDOUT WITH CSV HEADER;
 
 SELECT pg_store_plans_textplan(pg_store_plans_shorten($${
   "Plan": {
